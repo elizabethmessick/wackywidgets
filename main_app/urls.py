afc609e3e2b0c9b0ab5ab2b8f63f2b32b26abd7b
Elizabeth Messick <Elizabeth@MacBook-Pro-3.lan1>
2018-10-19 16:09:48 -0700

    update css on table

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('add_data/', views.add_data, name='add_data'),
+    path('delete_data/', views.delete_data, name='delete_data'),
+
 ]
